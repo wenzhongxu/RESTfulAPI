d3ad5fd7671046d4988c8a2c9861c56f72e5f2ca
wenzhongxu <xuwenzhong1994@163.com>
2021-01-24 22:39:04 +0800

    增加基础类

---
+++ Routine/Routine.Api/Entities/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Routine.Api.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+
+        public Guid CompanyId { get; set; }
+
+        public string EmployeeNo { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public DateTimeOffset DateOfBirth { get; set; }
+
+        public Company Company { get; set; }
+    }
+}
