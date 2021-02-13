700bb1d4b7787d0b49161cf8bb98ff98e09efe10
wenzhongxu <xuwenzhong1994@163.com>
2021-02-13 20:37:56 +0800

    增加种子数据

--- Routine/Routine.Api/Entities/Company.cs
+++ Routine/Routine.Api/Entities/Company.cs
@@ -13,6 +13,12 @@ namespace Routine.Api.Entities
 
         public string Introduction { get; set; }
 
+        public string Country { get; set; }
+
+        public string Industry { get; set; }
+
+        public string Product { get; set; }
+
         public ICollection<Employee> Employees { get; set; }
     }
 }
