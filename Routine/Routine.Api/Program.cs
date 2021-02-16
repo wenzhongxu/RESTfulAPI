2b3d2b20eefc8342e07f6b7a477fd7068f59ac7f
wenzhongxu <xuwenzhong1994@163.com>
2021-02-16 16:27:46 +0800

    数据库迁移

--- Routine/Routine.Api/Program.cs
+++ Routine/Routine.Api/Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Routine.Api.DbContexts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +16,26 @@ namespace Routine.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    var dbContext = scope.ServiceProvider.GetService<RoutineDbContext>();
+
+                    dbContext.Database.EnsureDeleted();
+                    dbContext.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "迁移数据库发生错误");
+                }
+            }
+
+            host.Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
