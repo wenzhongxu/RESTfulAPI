983d1d870a58a181d584e4494fba6cff9f23a5c2
wenzhongxu <xuwenzhong1994@163.com>
2021-03-14 21:16:44 +0800

    抽象共同验证

---
+++ Routine/Routine.Api/Models/EmployeeAddOrUpdateDto.cs
@@ -0,0 +1,41 @@
+﻿using Routine.Api.Entities;
+using Routine.Api.ValidationAttributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Routine.Api.Models
+{
+    [EmployeeNoMustDifferentFromFirstName(ErrorMessage = "员工编号和名必须不一样！")]
+    public abstract class EmployeeAddOrUpdateDto : IValidatableObject
+    {
+        [Display(Name = "员工号")]
+        [Required(ErrorMessage = "{0}是必填项")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "{0}的长度是{1}")]
+        public string EmployeeNo { get; set; }
+
+        [Display(Name = "名")]
+        [Required(ErrorMessage = "{0}是必填项")]
+        [MaxLength(50, ErrorMessage = "{0}的长度不能超过{1}")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "姓"), Required(ErrorMessage = "{0}是必填项"), MaxLength(50, ErrorMessage = "{0}的最大长度不能超过{1}")]
+        public string LastName { get; set; }
+
+        [Display(Name = "性别")]
+        public Gender Gender { get; set; }
+
+        [Display(Name = "出生日期")]
+        public DateTimeOffset DateOfBirth { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (FirstName == LastName)
+            {
+                yield return new ValidationResult("姓和名不能一致", new[] { nameof(EmployeeUpdateDto) });
+            }
+        }
+    }
+}
